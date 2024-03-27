04e1682f9e22d114df4b015e86275516275b63b4
DESKTOP-PNGU7CQ\tomke <tomkelly2547@gmail.com>
2024-03-27 09:21:47 +0000

    home page

---
+++ flutter_training/lib/views/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Posts')),
+      ),
+    );
+  }
+}
